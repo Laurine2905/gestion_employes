e69e22fbce56fa054123408c64ce6c0028e1d9ab
Laurine2905 <laurine.rat@outlook.fr>
2023-05-03 10:56:30 +0200

    Initial commit

---
+++ gestion_employes/backend/src/test/java/ehpad/gestion_employes/GestionEmployesApplicationTests.java
@@ -0,0 +1,13 @@
+package ehpad.gestion_employes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEmployesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
